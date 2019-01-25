c8bea2e5cb9edcf18b7767861934d31083918a38
Sergio Camara <scamara74@hotmail.com>
2019-01-25 15:02:36 -0300

    Acrescentada mensagem de boa noite

--- src/curso/Program.java
+++ src/curso/Program.java
@@ -3,8 +3,8 @@ package curso;
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.print("Bom dia");
-		System.out.println();
-		System.out.print("Boa tarde");
+		System.out.println("Bom dia");
+		System.out.println("Boa tarde");
+		System.out.println("Boa noite");
 	}
 }
